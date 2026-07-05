b8c05dd0ff00627d4e8fac6384d9ed900aee3c16
JP Duminy <jp@duminy.org>
2026-07-05 21:02:29 +0100

    Completed enums & strings.

--- exercises/09_strings/strings2.rs
+++ exercises/09_strings/strings2.rs
@@ -6,7 +6,7 @@ fn is_a_color_word(attempt: &str) -> bool {
 fn main() {
     let word = String::from("green"); // Don't change this line.
 
-    if is_a_color_word(word) {
+    if is_a_color_word(&word) { // rust automatically converts &String into &str
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
